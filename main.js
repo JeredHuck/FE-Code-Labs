e7151eaf21bbb577cec81c5cccd064c5489f4266
JeredHuck <jeredhuck@gmail.com>
2023-09-06 22:00:04 -0500

    Scoping Video 2
    This exercise demonstrates how const can be used to declare two different variables with the same name

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
-function user() {
-  let firstName = 'John'
-  let lastName = 'Smith'
-  let age = '25'
-  let country = 'England'
-  console.log(`${firstName} ${lastName} is ${age} years old and lives in ${country}.` )
-}
+ function test() {
+  const c = 2;
 
-user();
+  if(true) {
+    const c = 3;
+    console.log(c);
+  }
+  
+ };
 
-let firstName = 'James' // doesnt seem to work
-
-let game = 'Sonic';
-
-if(true) {              // it returns 'Mario'
-  let game = 'Mario';
-  console.log(game);
-}
-
-
-
+ test();
