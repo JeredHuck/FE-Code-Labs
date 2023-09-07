17c1a9fd5833c9dc8929ff698155961fb8e65b00
JeredHuck <jeredhuck@gmail.com>
2023-09-06 21:33:50 -0500

    Scoping Video 1
    I did all of the tasks of the first video here

---
+++ main.js
@@ -0,0 +1,21 @@
+function user() {
+  let firstName = 'John'
+  let lastName = 'Smith'
+  let age = '25'
+  let country = 'England'
+  console.log(`${firstName} ${lastName} is ${age} years old and lives in ${country}.` )
+}
+
+user();
+
+let firstName = 'James' // doesnt seem to work
+
+let game = 'Sonic';
+
+if(true) {              // it returns 'Mario'
+  let game = 'Mario';
+  console.log(game);
+}
+
+
+
