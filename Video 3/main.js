df3720ecf36a8284f6312365ba86773f406bb6ad
JeredHuck <jeredhuck@gmail.com>
2023-09-06 22:30:45 -0500

    Hoisting
    In this exercise, I completed both tasks from the third video

---
+++ Video 3/main.js
@@ -0,0 +1,14 @@
+// Task 1
+
+book ('The Magicians Nephew');
+
+function book(title) {
+  console.log(`The book is called ${title}.`)  // The book is called The Magicians Nephew
+}
+
+// Task 2
+console.log(book);
+
+let book = 'The Silver Chair'; // For the solution to this problem, I would declare the variable      
+                               //  before the console.log
+
