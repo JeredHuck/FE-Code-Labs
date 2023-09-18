538fdd8d267ea58887fa695006f0bfc42df50759
JeredHuck <jeredhuck@gmail.com>
2023-09-18 14:17:33 -0500

    Exercise 2
    9.18.23 Exercise 2

---
+++ 9.18.23 Exercises/Exercise 2/main.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn-inline');
+
+btn.addEventListener('click', changeBackground);
+
+function changeBackground() {
+  document.body.style.backgroundColor = '#35554e';
+}
